a6bbdef644d6557339107c35906417b608fa7abe
Carcab13 <boldtcarter@gmail.com>
2025-11-13 16:28:36 -0600

    Added Website

---
+++ SuprNova/src/Components/About/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+  return (
+    <section id="about" className="about-section">
+      <h2>About</h2>
+      <p>
+        This navbar is fixed to the top of the screen and includes smooth
+        scrolling navigation. It features a responsive design that adapts to
+        mobile devices with a hamburger menu.
+      </p>
+    </section>
+  )
+}
+
+export default About
+
---
+++ SuprNova/src/Components/Contact/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <section id="contact" className="contact-section">
+      <h2>Contact</h2>
+      <p>
+        Click on any navigation link to see the smooth scroll effect in action.
+        The navbar will remain fixed at the top as you scroll through the page.
+      </p>
+    </section>
+  )
+}
+
+export default Contact
+
